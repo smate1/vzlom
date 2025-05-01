a8b8be735d00ad9d129a4627f0aa276413ae48ee
Smate <xvava8442@gmail.com>
2025-05-01 18:56:10 +0200

    .

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $('.slides').slick({
 	slidesToShow: 1,
 	adaptiveHeight: true,
 	centerMode: true,
-	variableWidth: false,
+	// variableWidth: false,
 	centerPadding: '45px', // 45px зліва + 45px справа = 90px між слайдами
 	prevArrow:
 		'<button type="button" class="slick-prev"><img src="../images/arrow-left.svg" alt="Назад"></button>',
