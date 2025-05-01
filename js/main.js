91698696c9d921c2336320fdf49677eeb24b0abe
Smate <xvava8442@gmail.com>
2025-05-01 19:19:34 +0200

    /

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@ $('.slides').slick({
 	// variableWidth: false,
 	centerPadding: '45px', // 45px зліва + 45px справа = 90px між слайдами
 	prevArrow:
-		'<button type="button" class="slick-prev"><img src="./images/arrow-left.svg" alt="Назад"></button>',
+		'<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="Назад"></button>',
 	nextArrow:
-		'<button type="button" class="slick-next"><img src="./images/arrow-right.svg" alt="Вперед"></button>',
+		'<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="Вперед"></button>',
 })
 
