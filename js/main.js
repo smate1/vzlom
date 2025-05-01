de6f247f7cac567bf61b3cfbd3d8c9110dc165c3
Smate <xvava8442@gmail.com>
2025-05-01 18:02:08 +0200

    /

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$('.slides').slick({
+	dots: true,
+	infinite: true,
+	speed: 500,
+	slidesToShow: 1,
+	adaptiveHeight: true,
+	centerMode: true,
+	variableWidth: false,
+	centerPadding: '45px', // 45px зліва + 45px справа = 90px між слайдами
+	prevArrow:
+		'<button type="button" class="slick-prev"><img src="../images/arrow-left.svg" alt="Назад"></button>',
+	nextArrow:
+		'<button type="button" class="slick-next"><img src="../images/arrow-right.svg" alt="Вперед"></button>',
+})
