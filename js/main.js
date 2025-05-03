451e520de8a84bbb10cc391e3095542b871ffe73
Smate <xvava8442@gmail.com>
2025-05-03 14:44:31 +0200

    /

--- js/main.js
+++ js/main.js
@@ -12,4 +12,51 @@ $('.slides').slick({
 	nextArrow:
 		'<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="Вперед"></button>',
 })
+const customSelect = document.getElementById('custom-select')
+const trigger = customSelect.querySelector('.custom-select__trigger')
+const options = customSelect.querySelectorAll('.custom-option')
+const valueSpan = trigger.querySelector('span')
+
+trigger.addEventListener('click', () => {
+	customSelect.classList.toggle('open')
+})
+
+options.forEach(option => {
+	option.addEventListener('click', () => {
+		options.forEach(o => o.classList.remove('selected'))
+		option.classList.add('selected')
+		valueSpan.textContent = option.dataset.value
+		customSelect.classList.remove('open')
+	})
+})
+
+document.addEventListener('click', e => {
+	if (!customSelect.contains(e.target)) {
+		customSelect.classList.remove('open')
+	}
+})
+
+const footerSelect = document.getElementById('custom-select-footer')
+const footerTrigger = footerSelect.querySelector('.custom-select__trigger')
+const footerOptions = footerSelect.querySelectorAll('.custom-option')
+const footerValueSpan = footerTrigger.querySelector('span')
+
+footerTrigger.addEventListener('click', () => {
+	footerSelect.classList.toggle('open')
+})
+
+footerOptions.forEach(option => {
+	option.addEventListener('click', () => {
+		footerOptions.forEach(o => o.classList.remove('selected'))
+		option.classList.add('selected')
+		footerValueSpan.textContent = option.dataset.value
+		footerSelect.classList.remove('open')
+	})
+})
+
+document.addEventListener('click', e => {
+	if (!footerSelect.contains(e.target)) {
+		footerSelect.classList.remove('open')
+	}
+})
 
